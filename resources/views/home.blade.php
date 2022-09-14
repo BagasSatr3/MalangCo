32c1ad8f09a7051c4dbddcadfc5e2fa7aefb5af3
BagasSatr3 <103878193+BagasSatr3@users.noreply.github.com>
2022-09-14 08:04:54 +0700

    Commin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('template.main')
+
+@section('content')
+<div>
+    <div class="m-7 space-y-3 text-center bg-white rounded-lg border shadow-md sm:p-8">
+        <h5 class="text-xl font-bold text-gray-900 dark:text-white">
+            Wes Nggaweo Sak Karep Mu
+        </h5>
+    </div>
+</div>
