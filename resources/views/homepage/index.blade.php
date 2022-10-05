46f9ec3de89b5954cad688c271b88242a39ab365
Shinta Aishnabila <shintaaishnabila909@gmail.com>
2022-10-05 13:59:18 +0700

    fix : frond end home

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -39,110 +39,21 @@
   </div>
   <!-- end carousel -->
   <!-- kategori produk -->
-  <div class="row mt-4">
+  <!-- <div class="row mt-4">
     <div class="col col-md-12 col-sm-12 mb-4">
       <h2 class="text-center">Kategori Produk</h2>
     </div>
-    @foreach($itemkategori as $kategori)
+     -->
     <!-- kategori pertama -->
-    <div class="col-md-4">
-      <div class="card mb-4 shadow-sm">
-        <a href="{{ URL::to('kategori/'.$kategori->slug_kategori) }}">
-          @if($kategori->foto != null)
-          <img src="{{ \Storage::url($kategori->foto) }}" alt="{{ $kategori->nama_kategori }}" class="card-img-top">
-          @else
-          <img src="{{ asset('images/bag.jpg') }}" alt="{{ $kategori->nama_kategori }}" class="card-img-top">
-          @endif
-        </a>
-        <div class="card-body">
-          <a href="{{ URL::to('kategori/'.$kategori->slug_kategori) }}" class="text-decoration-none">
-            <p class="card-text">{{ $kategori->nama_kategori }}</p>
-          </a>
-        </div>
-      </div>
-    </div>
+    <!-- <div class="col-md-4">
 
-    @endforeach
+
+    
   <!-- end kategori produk -->
-  <!-- produk Promo-->
-  <div class="row mt-4">
-    <div class="col col-md-12 col-sm-12 mb-4">
-      <h2 class="text-center">Promo</h2>
-    </div>
-    @foreach($itempromo as $promo)
-    <!-- produk pertama -->
-    <div class="col-md-4">
-      <div class="card mb-4 shadow-sm">
-        <a href="{{ URL::to('produk/'.$promo->produk->slug_produk) }}">
-          @if($promo->produk->foto != null)
-          <img src="{{\Storage::url($promo->produk->foto) }}" alt="{{ $promo->produk->nama_produk }}" class="card-img-top">
-          @else
-          <img src="{{asset('images/bag.jpg') }}" alt="{{ $promo->produk->nama_produk }}" class="card-img-top">
-          @endif
-        </a>
-        <div class="card-body">
-          <a href="{{ URL::to('produk/'.$promo->produk->slug_produk) }}" class="text-decoration-none">
-            <p class="card-text">
-              {{ $promo->produk->nama_produk }}
-            </p>
-          </a>
-          <div class="row mt-4">
-            <div class="col">
-              <button class="btn btn-light">
-                <i class="far fa-heart"></i>
-              </button>
-            </div>
-            <div class="col-auto">
-              <p>
-                <del>Rp. {{ number_format($promo->harga_awal, 2) }}</del>
-                <br />
-                Rp. {{ number_format($promo->harga_akhir, 2) }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    @endforeach
+  
   <!-- end produk promo -->
   <!-- produk Terbaru-->
-  <div class="row mt-4">
-    <div class="col col-md-12 col-sm-12 mb-4">
-      <h2 class="text-center">Terbaru</h2>
-    </div>
-    @foreach($itemproduk as $produk)
-    <!-- produk pertama -->
-    <div class="col-md-4">
-      <div class="card mb-4 shadow-sm">
-        <a href="{{ URL::to('produk/satu') }}">
-          @if($produk->foto != null)
-          <img src="{{ \Storage::url($produk->foto) }}" alt="{{ $produk->nama_produk }}" class="card-img-top">
-          @else
-          <img src="{{ asset('images/bag.jpg') }}" alt="{{ $produk->nama_produk }}" class="card-img-top">
-          @endif
-        </a>
-        <div class="card-body">
-          <a href="{{ URL::to('produk/'.$produk->slug_produk ) }}" class="text-decoration-none">
-            <p class="card-text">
-              {{ $produk->nama_produk }}
-            </p>
-          </a>
-          <div class="row mt-4">
-            <div class="col">
-              <button class="btn btn-light">
-                <i class="far fa-heart"></i>
-              </button>
-            </div>
-            <div class="col-auto">
-              <p>
-                Rp. {{ number_format($produk->harga, 2) }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    @endforeach
+  
   <!-- end produk terbaru -->
   <!-- tentang toko -->
   <hr>
@@ -165,6 +76,7 @@
 
 
 <!-- kategori produk -->
+@foreach($itemkategori as $kategori)
 <div class="kategori">
   <div class="row mt-4">
     <div class="col col-md-12 col-sm-12 mb-4">
@@ -216,6 +128,7 @@
 </div>
       </div>
     </div>
+    @endforeach
     <!-- kategori ketiga -->
     
   </div>
@@ -294,6 +207,7 @@
       </div>
     </div>
 </div>
+@endforeach
   </div>
   </br>
 </br>
