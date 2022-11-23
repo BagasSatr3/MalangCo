9350897e8ef855cbde859d7312813abf1f73f87b
siraxel <axedim085@gmail.com>
2022-11-23 12:55:23 +0700

    push
    ganti footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,60 @@
-<div class="container">
-  <div class="row">
-    <div class="col">
-    <hr />
-      <footer class="bg-transparent text-center text-white">
-  <!-- Grid container -->
-  <div class="container">
-    <!-- Section: Social media -->
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-transparant text-muted ">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="mdb-5 d-none d-lg-block" style="">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
     
-  </div>
-  <!-- Grid container -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>KOJO
+          </h6>
+          <p>
+            Selamat datang di Kojo!
+            Sebuah platform jual beli berbagai macam
+            alat dan bahan perkebunan yang berkualitas
+          </p>
+        </div>
+        <!-- Grid column -->
+
+       
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> SMK Negeri 8 Malang</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 628 1234 5678</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
   <!-- Copyright -->
-  <div class="text-center p-3" style="color:black;">
-    © 2022 KOJO
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright: KOJO
   </div>
   <!-- Copyright -->
 </footer>
-    </div>
-  </div>
-</div>
+<!-- Footer -->
